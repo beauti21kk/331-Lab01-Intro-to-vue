9cbff20530b16bd4c003ac653cd6a1fb83b0f20a
beauti21kk <kornkanok_kanchana@cmu.ac.th>
2023-06-30 01:14:12 +0700

    2.4

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { createApp, ref} = Vue
 
 createApp({
     setup(){
-        const product = ref('Socks')
+        const product = ref('Boots')
         return {
             product
         }

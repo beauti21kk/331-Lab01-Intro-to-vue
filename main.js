a8be0831993e8f19e67d5504876b63cfbd27f736
beauti21kk <kornkanok_kanchana@cmu.ac.th>
2023-06-30 09:47:35 +0700

    2.5

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ createApp({
     setup(){
         const product = ref('Boots')
         const image = ref('./assets/images/socks_green.jpg')
+        const description = ref('This product has many colors.')
         return {
             product,
+            description,
             image
         }
     }

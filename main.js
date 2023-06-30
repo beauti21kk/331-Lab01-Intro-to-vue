dc0fbd63b038361364a61858e59f68d471645256
beauti21kk <kornkanok_kanchana@cmu.ac.th>
2023-06-30 21:59:36 +0700

    6.6

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ createApp({
             '20% polyester'
         ])
         const variants = ref([
-            {id: 2234, color: 'green' },
-            {id: 2235, color: 'blue' }
+            {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+            {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
         ])
         const sizes = ref([
             'S', 'M', 'L'
@@ -24,6 +24,9 @@ createApp({
         const cart = ref(0)
         function addToCart(){
             cart.value +=1
+        }
+        function updateImage(variantImage){
+            image.value = variantImage
         }
         return {
             product,
@@ -37,7 +40,8 @@ createApp({
             variants,
             sizes,
             cart,
-            addToCart
+            addToCart,
+            updateImage
         }
     }
 }).mount('#app')

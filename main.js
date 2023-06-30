f3b8f9b080f32d9408b1952590ee551bb2989fea
beauti21kk <kornkanok_kanchana@cmu.ac.th>
2023-06-30 10:01:07 +0700

    3.6

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ createApp({
     setup(){
         const product = ref('Boots')
         const image = ref('./assets/images/socks_green.jpg')
+        const url = ref('https://www.camt.cmu.ac.th')
         const description = ref('This product has many colors.')
         return {
             product,
+            url,
             description,
             image
         }

cb5f59b12193688a5ca7ece153f44af44d98da36
beauti21kk <kornkanok_kanchana@cmu.ac.th>
2023-06-30 10:36:14 +0700

    5.3

--- main.js
+++ main.js
@@ -14,6 +14,10 @@ createApp({
             '30% wool',
             '20% polyester'
         ])
+        const variants = ref([
+            { id: 2234, color: 'green' },
+            { id: 2235, color: 'blue' }
+        ])
         return {
             product,
             url,
@@ -22,7 +26,8 @@ createApp({
             inStock,
             inventory,
             sale,
-            details
+            details,
+            variants
         }
     }
 }).mount('#app')

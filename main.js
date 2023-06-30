9c932c927b9b1c6befa527049ff52c513a3fd85a
beauti21kk <kornkanok_kanchana@cmu.ac.th>
2023-06-30 10:05:57 +0700

    4.5

--- main.js
+++ main.js
@@ -7,12 +7,14 @@ createApp({
         const url = ref('https://www.camt.cmu.ac.th')
         const description = ref('This product has many colors.')
         const inStock = ref(true)
+        const inventory = ref(100)
         return {
             product,
             url,
             description,
             image,
-            inStock
+            inStock,
+            inventory
         }
     }
 }).mount('#app')

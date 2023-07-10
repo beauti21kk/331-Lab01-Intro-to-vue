2c97b5c29f2d496a3420051a945e26ad537ccc5c
beauti21kk <kornkanok_kanchana@cmu.ac.th>
2023-07-10 16:59:38 +0700

    11.7

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -23,7 +23,7 @@ const productDisplay = {
             <button class="button" :disabled='!inStock' @click="addToCart" :class="{disabledButton: !inStock}">Add To Cart</button>
             <button class="button" @click="removeCart">Remove</button>
             </div>
-            <review-list :reviews="reviews"></review-list>
+            <review-list v-if="reviews.length" :reviews="reviews"></review-list>
             <review-form @review-submitted="addReview"></review-form>
         </div>
     `,

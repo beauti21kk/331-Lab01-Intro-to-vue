817386b29f5358c7fd3acfb0a668aa8dede38302
beauti21kk <kornkanok_kanchana@cmu.ac.th>
2023-07-10 17:02:30 +0700

    11.8

--- components/ReviewForm.js
+++ components/ReviewForm.js
@@ -27,6 +27,10 @@ const reviewForm = {
             rating: null
         })
         function onSubmit(){
+            if (form.name == '' || form.review == '' || form.rating == null){
+                alert('Review is incomplete. Please fill out every field.')
+                return
+            }
             const productReview = {
                 name: form.name,
                 review: form.review,

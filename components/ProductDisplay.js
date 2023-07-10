058aedec9cc4a0dda1928dda3f153708170a8186
beauti21kk <kornkanok_kanchana@cmu.ac.th>
2023-07-10 16:21:02 +0700

    11.2

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -23,6 +23,7 @@ const productDisplay = {
             <button class="button" :disabled='!inStock' @click="addToCart" :class="{disabledButton: !inStock}">Add To Cart</button>
             <button class="button" @click="removeCart">Remove</button>
             </div>
+            <review-form></review-form>
         </div>
     `,
     props:{
